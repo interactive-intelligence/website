9a3e1f72d716f352b0e198d4e09ebf4eb53b0f10
refact0r <34758569+refact0r@users.noreply.github.com>
2026-04-26 00:27:13 -0700

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://interactive-intelligence.github.io',
+	base: '/website',
+});
