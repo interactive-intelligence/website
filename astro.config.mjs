7674efa9d44c8fa22ecbf4d762b41a313055c0d8
refact0r <34758569+refact0r@users.noreply.github.com>
2026-05-13 13:21:24 -0700

    add linkable headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://interactive-intelligence.github.io',
 	base: '/website/',
 	integrations: [icon()],
+	markdown: {
+		rehypePlugins: [
+			rehypeSlug,
+			[
+				rehypeAutolinkHeadings,
+				{
+					behavior: 'append',
+					properties: { className: 'heading-anchor', ariaLabel: 'Link to section' },
+					content: { type: 'text', value: '#' },
+				},
+			],
+		],
+	},
 });
